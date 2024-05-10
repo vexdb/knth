885e515e50b09becdb1aefc6f1889112899a5af2
felipecruz <felipecruz@gmail.com>
2024-05-10 21:23:07 +0200

    Fix nth_nk and remove array length parameter

--- src/knth.nim
+++ src/knth.nim
@@ -3,6 +3,7 @@ import std/strformat
 
 import knth/nth_quickselect
 import knth/nth_nk
+import knth/nth_mom
 
 proc knth_n_quickselect*[T](a: var openArray[T], k: int): T =
   if k > a.len:
@@ -15,4 +16,10 @@ proc knth_nk*[T](a: var openArray[T], k: int): T =
   if k > a.len:
     raise newException(ValueError, &"k {k} is out of bounds")
 
-  return iknth_nk(a, a.len, k)
+  return iknth_nk(a, k)
+
+proc knth_quickselect_mom*[T](a: var openArray[T], k: int): T =
+  if k > a.len:
+    raise newException(ValueError, &"k {k} is out of bounds")
+
+  return iknth_select(a, k - 1)
--- src/knth/nth_nk.nim
+++ src/knth/nth_nk.nim
@@ -1,22 +1,25 @@
-proc iknth_nk*[T](a: openArray[T], length: int, k: int): T =
-  var aux: seq[T] = newSeq[T](length + 1)
+proc iknth_nk*[T](a: openArray[T], k: int): T =
+  var aux: seq[bool] = newSeq[bool](a.len)
   var min = low(T)
   var number: T
   var current_min_index = -1
+
+  if a.len == 0:
+    return a[0]
     
   for i, x in aux:
-    aux[i] = T(0)
+    aux[i] = false
 
   for i in 0 ..< k:
     min = high(T)
 
-    for j in i ..< length:
+    for j in 0 ..< a.len:
       number = a[j]
 
-      if aux[j] == 0 and number < min:
+      if not aux[j] and number < min:
         min = number
         current_min_index = j
 
-    aux[current_min_index] = min
+    aux[current_min_index] = true
   
   return min
